df6df6455d74b26c69ea5b2bf47007aef3a16bfe
sanujsood <67072668+sanujsood@users.noreply.github.com>
2021-10-21 23:36:32 +0530

    Hero Section

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import { PersonalInfo } from '../components'
-
+import { Hero } from '../components'
 
 
 
 export default function Home() {
   return (
     <>
+      <Hero></Hero>
       <PersonalInfo></PersonalInfo>
     </>
   )
