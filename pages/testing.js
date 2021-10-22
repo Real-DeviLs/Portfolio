bb68da9a7378afe2f2ba7cf7e054702888ba6e11
muskan <msoni00005@gmail.com>
2021-10-22 14:38:49 +0530

    adds blogcard component

--- pages/testing.js
+++ pages/testing.js
@@ -2,14 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 
-import { PersonalInfo,Biography,ContactUs,Accordian } from '../components/'
+import { PersonalInfo,Biography,ContactUs,Accordian,BlogCard } from '../components/'
 
  
 
 export default function Home() {
   return (
       <>
-      <Accordian/>
+      <BlogCard/>
+      {/* <Accordian/> */}
       {/* <ContactUs/> */}
       {/* <PersonalInfo></PersonalInfo> */}
       {/* <Biography></Biography> */}
